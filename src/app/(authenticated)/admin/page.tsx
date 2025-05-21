8b546815165f0a0d64cb72b3bfe96440246adfa9
philbert816 <chengchaiman816@gmail.com>
2025-05-22 03:50:23 +0800

    Created supabase bucket and RLS and also file upload to bucket in admin

--- src/app/(authenticated)/admin/page.tsx
+++ src/app/(authenticated)/admin/page.tsx
@@ -1,5 +1,47 @@
-const page = () => {
-  return <div>Admin Page</div>;
+"use client";
+import { createClient } from "@/utils/supabase/client";
+import { useState, useRef } from "react";
+
+const Page = () => {
+  const [file, setFile] = useState<File | null>(null);
+  const fileInputRef = useRef<HTMLInputElement>(null);
+  const supabase = createClient();
+
+  return (
+    <div className="py4 h-full w-full py-4 pr-4 pl-8">
+      <h2 className="mb-2 text-xl font-bold">Upload Judgements</h2>
+      <input
+        ref={fileInputRef}
+        name="file"
+        onChange={(e) => setFile(e.target.files?.[0] || null)}
+        className="bg-background ring-offset-background placeholder:text-muted-foreground focus-visible:ring-ring mb-2 flex h-10 cursor-pointer rounded-md border border-gray-300 px-3 py-2 text-sm file:border-0 file:bg-transparent file:text-sm file:font-medium focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:outline-none disabled:cursor-not-allowed disabled:opacity-50"
+        type="file"
+      />
+      <button
+        onClick={async () => {
+          //Check if have file
+          if (file) {
+            // Upload file to supabase storage
+            // TODO: put this into a server action
+            await supabase.storage
+              .from("judgements")
+              //Bucket RLS policy checks for the second path token to be a valid uuid
+              //IF want to change need to change the RLS policy as well
+              .upload(`${file.name}/${crypto.randomUUID()}`, file);
+
+            // Reset file state and input
+            setFile(null);
+            if (fileInputRef.current) {
+              fileInputRef.current.value = "";
+            }
+          }
+        }}
+        className="cursor-pointer rounded-md bg-blue-500 px-4 py-1 text-white"
+      >
+        Upload
+      </button>
+    </div>
+  );
 };
 
-export default page;
+export default Page;
---
+++ supabase/migrations/20250521170619_files.sql
@@ -0,0 +1,59 @@
+-- Create private schema, public schema is accessable on client side
+-- private schema is accessable on server side only
+create schema private;
+
+-- Create bucket for files
+insert into storage.buckets (id, name)
+values ('judgements', 'judgements')
+on conflict do nothing;
+
+-- Function for checking if the text is a valid uuid
+create or replace function private.uuid_or_null(str text)
+returns uuid
+language plpgsql
+as $$
+begin
+  return str::uuid;
+  exception when invalid_text_representation then
+    return null;
+  end;
+$$;
+
+create policy "Users can upload files to judgements bucket if they are admin"
+on storage.objects for insert to authenticated with check (
+  bucket_id = 'judgements' 
+  and owner = auth.uid()
+  and exists (
+    select 1 from user_profile 
+    where id = auth.uid() 
+    and role = 'admin'
+  )
+  -- Check if the first path token is a valid uuid
+  -- Postgres SQL array indexing starts at 1
+  and private.uuid_or_null(path_tokens[2]) is not null
+);
+
+create policy "Users can update files to judgements bucket if they are admin"
+on storage.objects for update to authenticated using  (
+  bucket_id = 'judgements' 
+  and exists (
+    select 1 from user_profile 
+    where id = auth.uid() 
+    and role = 'admin'
+  )
+);
+
+create policy "Users can delete files from judgements bucket if they are admin"
+on storage.objects for delete to authenticated using (
+  bucket_id = 'judgements' 
+  and exists (
+    select 1 from user_profile 
+    where id = auth.uid() 
+    and role = 'admin'
+  )
+);
+
+create policy "Authenticated users can view all files from judgements bucket"
+on storage.objects for select to authenticated using (
+  bucket_id = 'judgements'
+);
